01203be6a7189c45cdef718a2f4fcb477e188ac1
mirkojvct <136806389+mirkojvct@users.noreply.github.com>
2024-03-13 14:08:54 +0100

    task5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://phelxcsd0i.execute-api.eu-west-1.amazonaws.com",
   bff: "https://llj0do5ze0.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
